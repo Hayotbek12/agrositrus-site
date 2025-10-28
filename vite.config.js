33b3f8d1b3a9b21ae6ce1957e2f4e36cc88a8127
Hayotbek12 <shundayshunday988@gmail.com>
2025-10-28 16:55:50 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
-  root: '.', // index.html shu papkada
-  base: './', // nisbiy yo'l
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
     rollupOptions: {
-      input: './index.html'
+      input: resolve(__dirname, 'index.html')
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
     }
   }
 })
