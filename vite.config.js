19216ecbb3321cf1e1e913a0c311e91ee9bcac7d
Hayotbek12 <shundayshunday988@gmail.com>
2025-10-28 16:49:49 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: '.',              // <-- bu muhim
   plugins: [react()],
-  build: { outDir: 'dist' }
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: './index.html'  // <-- bu ham kerak
+    }
+  }
 })
